f87c845e91815e44a58b3b2b6f21e0f5755a71e9
Daniil Yarockii <yaroc.d@gmail.com>
2022-02-22 16:58:53 +0300

    config updated

--- src/main.py
+++ src/main.py
@@ -16,8 +16,10 @@ import src.batched_smart_tool as batched_smart_tool
 import src.settings_card as settings_card
 import src.grid_controller as grid_controller
 
+from src.sly_globals import app
 
-@g.app.get("/")
+
+@app.get("/")
 def read_index(request: Request):
     return g.templates_env.TemplateResponse('index.html', {'request': request,
                                                            'smart_tool': SmartTool})
@@ -25,20 +27,20 @@ def read_index(request: Request):
 
 # @TODO: ...
 
-if __name__ == "__main__":
-    g.app.add_api_route('/connect-to-model/{identifier}', settings_card.connect_to_model, methods=["POST"])
-    g.app.add_api_route('/select-input-project/{identifier}', settings_card.select_input_project, methods=["POST"])
-    g.app.add_api_route('/select-output-project/{identifier}', settings_card.select_output_project, methods=["POST"])
-    g.app.add_api_route('/select-output-class/{identifier}', settings_card.select_output_class, methods=["POST"])
 
-    g.app.add_api_route('/windows-count-changed/', grid_controller.windows_count_changed, methods=["POST"])
+app.add_api_route('/connect-to-model/{identifier}', settings_card.connect_to_model, methods=["POST"])
+app.add_api_route('/select-input-project/{identifier}', settings_card.select_input_project, methods=["POST"])
+app.add_api_route('/select-output-project/{identifier}', settings_card.select_output_project, methods=["POST"])
+app.add_api_route('/select-output-class/{identifier}', settings_card.select_output_class, methods=["POST"])
 
-    g.app.add_api_route('/change-all-buttons/{is_active}', batched_smart_tool.change_all_buttons, methods=["POST"])
-    g.app.add_api_route('/clean-points/', batched_smart_tool.clean_points, methods=["POST"])
-    g.app.add_api_route('/update-masks/', batched_smart_tool.update_masks, methods=["POST"])
-    g.app.add_api_route('/next-batch/', batched_smart_tool.next_batch, methods=["POST"])
+app.add_api_route('/windows-count-changed/', grid_controller.windows_count_changed, methods=["POST"])
 
-    g.app.add_api_route('/widgets/smarttool/negative-updated/{identifier}', batched_smart_tool.points_updated, methods=["POST"])
-    g.app.add_api_route('/widgets/smarttool/positive-updated/{identifier}', batched_smart_tool.points_updated, methods=["POST"])
+app.add_api_route('/change-all-buttons/{is_active}', batched_smart_tool.change_all_buttons, methods=["POST"])
+app.add_api_route('/clean-points/', batched_smart_tool.clean_points, methods=["POST"])
+app.add_api_route('/update-masks/', batched_smart_tool.update_masks, methods=["POST"])
+app.add_api_route('/next-batch/', batched_smart_tool.next_batch, methods=["POST"])
 
-    uvicorn.run(g.app, host="127.0.0.1", port=8000)
+app.add_api_route('/widgets/smarttool/negative-updated/{identifier}', batched_smart_tool.points_updated, methods=["POST"])
+app.add_api_route('/widgets/smarttool/positive-updated/{identifier}', batched_smart_tool.points_updated, methods=["POST"])
+
+    # uvicorn.run(app, host="127.0.0.1", port=8000)
