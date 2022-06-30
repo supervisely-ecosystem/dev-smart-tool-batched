a1577b7c449b579b353c6c935ed5e423eb281eeb
Daniel Y <33942379+qanelph@users.noreply.github.com>
2022-06-30 16:04:20 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,12 +11,20 @@ from smart_tool import SmartTool  # 🤖 widgets
 import src.initialize_app as initialize_app
 import src.sly_globals as g
 from supervisely.app import StateJson, DataJson
+from supervisely.app.fastapi import available_after_shutdown
 
 
 @g.app.get("/")
-def read_index(request: Request):
+@available_after_shutdown(app=g.app)
+def read_index(request: Request = None):
     return g.templates_env.TemplateResponse('index.html', {'request': request,
                                                            'smart_tool': SmartTool})
+
+
+@g.app.on_event("shutdown")
+def shutdown():
+    read_index()  # save last version of static files
+
 
 
 @g.app.on_event("startup")
